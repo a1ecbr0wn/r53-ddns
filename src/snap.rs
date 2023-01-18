9ae1fc85b2347d4433449aa4a094724a704f8c3c
Alec Brown <12213854+a1ecbr0wn@users.noreply.github.com>
2023-01-18 22:19:54 +0000

    Log file location (#23)
    * `-l` parameter to set the location for the log file
    Release v0.1.10
    
    * Format Rust code using rustfmt
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/snap.rs
+++ src/snap.rs
@@ -1,11 +1,11 @@
 use std::env;
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 
 /// Checks to see whether we are operating within a snap
 pub(crate) fn check_in_snap() -> (bool, Option<PathBuf>) {
     if env::var("SNAP_NAME").is_ok() {
         if let Ok(host_home_str) = env::var("SNAP_REAL_HOME") {
-            (true, Some(Path::new(host_home_str.as_str()).to_path_buf()))
+            (true, Some(PathBuf::from(host_home_str.as_str())))
         } else {
             (true, None)
         }
